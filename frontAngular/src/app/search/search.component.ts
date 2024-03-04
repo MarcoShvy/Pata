b1af0a6e5362478136b279f85ed2121d05a2b835
Marco Antonio Da Silva Filho <20220725@sarah.br>
2024-03-04 15:22:13 -0300

    search changes

--- frontAngular/src/app/search/search.component.ts
+++ frontAngular/src/app/search/search.component.ts
@@ -10,20 +10,9 @@ export class SearchComponent {
   specie: string = '';
   species: string[] = ['cachorro', 'Gato'];
   raceDog: string[] = ['Caramelo', 'Auau'];
-  raceCat: string[] = ['Siames', 'Persa']
   race: string = ''
 
   
-
-  raceSelect() {
-    if (this.specie == 'cachorro') {
-      return this.raceDog
-    } else {
-    return this.raceCat
-  }
-  }
-
-
   colors: string[] = ['white', 'flamingo', 'pínk'];
 
 }
